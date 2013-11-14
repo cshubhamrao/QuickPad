16b57cbb602323d885e197db83b2b4c0d87e6321
Shubham Rao <cshubhamrao@gmail.com>
2013-11-14 20:09:46 +0530

    Change Look&Feel to System Specific

--- JQuickPad/src/jquickpad/QuickPadUI.java
+++ JQuickPad/src/jquickpad/QuickPadUI.java
@@ -5,6 +5,7 @@
 package jquickpad;
 
 import java.io.*;
+import javax.swing.UIManager;
 
 /**
  *
@@ -142,20 +143,14 @@ public static void writeData(javax.swing.JFileChooser fileChooser,
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(QuickPadUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(QuickPadUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(QuickPadUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(QuickPadUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+             UIManager.setLookAndFeel(
+            UIManager.getSystemLookAndFeelClassName());
+                
+            
+        } catch (ClassNotFoundException | InstantiationException | 
+                IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(QuickPadUI.class.getName()).log
+                    (java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
